c7de7347a3a1d8f792a98484c355924c8ee6df15
Balogun Dotun <balogunlightonola@gmail.com>
2025-09-12 14:31:54 +0100

    feat:home -service card

--- src/components/ui/Card/ServiceCard.tsx
+++ src/components/ui/Card/ServiceCard.tsx
@@ -1,8 +1,21 @@
 
-const ServiceCard = () => {
+type serviceCardProps = {
+  image: string;
+  title: string;
+};
+const ServiceCard = ({ image, title }: serviceCardProps) => {
   return (
-    <div>ServiceCard</div>
-  )
-}
+    <div className="relative group rounded-[8px] bg-white hover:bg-accent hover:translate-y-5 transition-all duration-100 ease-in-out text-center w-80 h-45 md:w-80 lg:w-50 md:h-65 shadow-2xl py-4 px-2 md:px-6 md:py-10 flex flex-col gap-4 lg:gap-8 items-center">
+      <img src={image} alt="service image" className=" w-15 h-15 md:w-20 md:h-20 transition-transform duration-800 ease-in-out group-hover:invert group-hover:brightness-20  group-hover:rotate-y-360" />
+     
+      <h3 className="text-gray-700 text-base font-bold">{title}</h3>
+      <div className="absolute   left-1/2    top-full transform -translate-x-1/2 -translate-y-1/2">
+        <button className="bg-accent  py-2 w-40  group-hover:bg-white group-hover:text-accent rounded text-white font-bold">
+          More details
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,47 @@
-import Herosection from '../features/home/components/Herosection'
+import ServiceCard from "../components/ui/Card/ServiceCard";
+import Herosection from "../features/home/components/Herosection";
+import icon1 from "../assets/icon-1.png";
+import icon2 from "../assets/icon-2.png";
+import icon3 from "../assets/icon-3.png";
+import icon4 from "../assets/icon-4.png";
+import icon5 from "../assets/icon-5.png";
+
+type serviceProps = {
+  id: number;
+  name: string;
+  path: string;
+};
+const services: serviceProps[] = [
+  { id: 1, name: "Digestive System", path: icon1 },
+  { id: 2, name: "Urologist Clinic", path: icon2 },
+  { id: 2, name: "Cardiologist Clinic", path: icon3 },
+  { id: 2, name: "Endocrinologist Clinic", path: icon4 },
+  { id: 2, name: "Dentistry Clinic", path: icon5 },
+  { id: 2, name: "Dentistry Clinic", path: icon5 },
+];
 
 const Home = () => {
   return (
     <div>
-        <Herosection/>
+      <Herosection />
+      <section className="relative min-h-screen bg-primary">
+        <div className="max-w-7xl mx-auto py-20 px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 place-items-center place-content-stretch py-2 gap-y-12">
+            {services.map((service) => (
+              <ServiceCard
+              key={service.id}
+              image={service.path}
+              title={service.name}
+               />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className='absolute bg-gray-700 top-2 min-h-screen'>
+d
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
