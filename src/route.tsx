ca9c1ea61e66bf58011cfd4f735d8bd2ccabe244
Balogun Dotun <balogunlightonola@gmail.com>
2025-09-21 23:27:50 +0100

    added errorElemet to dynamic layout

--- src/route.tsx
+++ src/route.tsx
@@ -21,6 +21,7 @@ const routes:RouteObject[] = [
       
       {
         element:<DynamicLayout/>,
+        errorElement:<NotFound/>,
         children:[
           {path:'/about',
             element:<About/>
